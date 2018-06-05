4e45dd1135d4bd8945f1dca51a71b1991fcc3aa3
Joris Witteman <joriswitteman@hotmail.nl>
2018-06-05 13:26:37 +0200

    Fixed formatting, want jorn kan er niks van

--- classes/questions.py
+++ classes/questions.py
@@ -72,85 +72,85 @@ What is the general expression for the lorentz force that is exerted by an elect
   B: F = qE * qv * B
   C: F = qE * qv + B
   D: F = qE + qv * B"""]
-  
+
 questionsDiff = ["""Electromagnetics II (hard):
 Consider the time-harmonic electric field component of a uniform plane wave, given by E=E0sin(ωt−k0x+ϕ)azV/m
 Give an expression for the corresponding magnetic field component.
   A: H=−Z−10E0cos(ωt−k0x+ϕ)ayA/m
-X B: H=−Z−10E0sin(ωt−k0x+ϕ)ayA/m
+  B: H=−Z−10E0sin(ωt−k0x+ϕ)ayA/m
   C: H=−Z0E0sin(ωt−k0x+ϕ)ayA/m
   D: the specified electric field is not consistent with Maxwell’s equations""",
-  
+
 """Mathematics II(hard):
 Random variableXis uniform(−1,3) distributed.  Furthermore, letY=X2. For 1≤y≤9, the probability P(Y≤y) is given by
   A:12√y
   B:12(√y−1)
   C:14(√y−1)
-X D:14(√y+ 1)""",
+  D:14(√y+ 1)""",
 
 """Electronic Circuits II(hard):
 Calculate the effective capacitance and give the connections of the Miller capacitance. The Miller capacitance is 10uF and the gain of the MOSFET is 10x.
-X A:Gate-Drain,110uF
+  A:Gate-Drain,110uF
   B:Source-Drain,110uF
   C:Gate-Drain,11uF
   D:Source-Drain,100uF""",
 
 """Control Systems(hard):
-Consider a feedback control scheme , where G(s) =1s2+ 8s+ 7.A. Consider the controller D(s) = 1s(s+ 10).Compute the steady-state tracking error of the resulting closed loop system for a parabolic input.  Hint: use the final value theorem.			
+Consider a feedback control scheme , where G(s) =1s2+ 8s+ 7.A. Consider the controller D(s) = 1s(s+ 10).Compute the steady-state tracking error of the resulting closed loop system for a parabolic input.  Hint: use the final value theorem.
   A:55
   B:20
-X C:70
+  C:70
   D:2]""",
-  
+
 """Electromechanics(hard):
 A series dc motor that can be treated as magnetically linear is operating at 1200 rpm with
 Vt = 250 V and Ia = 50 A. It is known that Ra + Rs = 0.2 Ω. If torque is increased until the speed is reduced to 750 rpm while Vt remains constant in value, determine the new value for Td.
-	A:180.11Nm
-	B:259.20Nm
-	C:161.61Nm
-X	D:233.14Nm""",
+  A:180.11Nm
+  B:259.20Nm
+  C:161.61Nm
+  D:233.14Nm""",
 
 """Wireless Components and Technlogies(hard):
 Consider two celltowers 3 kilometers apart. One towers sends a message with a signals strength of 500mW at 5GHz. The antenna's have unit gain. What is the power received at the other antenna?
-	A:1.00pW
-X	B:1.25pW
-	C:2.00pW
-	D.3.14pW""",
-	
+  A:1.00pW
+  B:1.25pW
+  C:2.00pW
+  D.3.14pW""",
+
 """Computation II(hard):
-Imagine that you have trained your St. Bernard, Korg, to carry all Iron Man movies on Blu-Ray DVD's instead of Batman's. Each Blu-Ray DVD contains 5e10 bytes. The alien runs at 36 km/hour, but because he is made of stone and perpetually out of shape, every 10 seconds he needs to stop and rest for 5 seconds. 
+Imagine that you have trained your St. Bernard, Korg, to carry all Iron Man movies on Blu-Ray DVD's instead of Batman's. Each Blu-Ray DVD contains 5e10 bytes. The alien runs at 36 km/hour, but because he is made of stone and perpetually out of shape, every 10 seconds he needs to stop and rest for 5 seconds.
 
 What is the average datarate if Korg runs for 4000 meters? For simplicity, also take the last 5 seconds break when he reached his destination into account.
-	A:2Gb/s
-	B:1Gb/s
-	C:3Gb/s
-	D:5Gb/s""",
+  A:2Gb/s
+  B:1Gb/s
+  C:3Gb/s
+  D:5Gb/s""",
 
 """Power Electonics(hard):
 Figure https://imgur.com/qU709gu shows a step-up converter.  The input voltage Vd = 30V.  The converter is controlled to give a constant output voltage Vo= 80 V.  The inductance is L= 275μH. The switching frequency is 50kHz. The load resistance is 420Ω, in what mode of operation is this converter?
-	A: CCM
-	B: BCM
-X	C: DCM
-	D: None""",
-	
+  A: CCM
+  B: BCM
+  C: DCM
+  D: None""",
+
 """Transmissionslines(hard):
 Consider a transmissionline with lenght L. The line impedance is 25Ω and it is terminated by a 75Ω load. The frequency is 3THz. What is the reflection coefficient?
-	A:1
-X	B:0.5
-	C:0
-	D:π""",
-	
+  A:1
+  B:0.5
+  C:0
+  D:π""",
+
 """Thor(hard):
 In the Marvel movies, what is the name of the guy with the red suit powered by a miniaturized Arc Reactor?
-	A: Captain America
-	B: Thor
-	C: Iron Man
-X	D: The Hulk"""]
-	
-	
+  A: Captain America
+  B: Thor
+  C: Iron Man
+  D: The Hulk"""]
 
 
-  
+
+
+
 answerEasy = ["B", "B", "A", "A", "C", "B", "D", "C", "C", "D"]
 answerDiff = ["B", "D", "A", "C", "D", "B", "A", "C", "B", "D"]
 codes = ["forecast", "neighbour", "pledge", "drawing", "attitude", "proclaim", "judgment", "mother", "forestry", "disorder"]
