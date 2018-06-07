861390ff37a78bb49c6028db176ed8a92e1e852c
Joris Witteman <joriswitteman@hotmail.com>
2018-06-07 11:09:43 +0200

    FIxed dicts

--- classes/questions.py
+++ classes/questions.py
@@ -3,77 +3,77 @@
 #
 # Simple Bot to reply to Telegram messages
 # This program is dedicated to the public domain under the CC0 license.
-questionsEasy = ["""Computation I (easy):
+questionsEasy = {'0':"""Computation I (easy):
 A CPU runs at 2 GHz with a CPI of 1.5. If a program runs for 15 seconds, how many instructions are then executed?
   A: 10*10^9
   B: 20*10^9
   C: 10*10^6
   D: 20*10^6""",
 
-"""Mathematics I (easy):
+'1':"""Mathematics I (easy):
 The limit lim(x,y)->(1,1) (x^(2)*y-x*y^(2))/(x^(2)-y^(2))
   A: has value 0
   B: has value 1/2
   C: has value 1
   D: does not exist""",
 
-"""USE (easy):
+'2':"""USE (easy):
 What engineering intervention was part motorist advocates' vision?
   A: Fast lanes for motorized vehicles
   B: New traffic laws
   C: Driving tests and licenses for motorists
   D: Educational campaigns for pedestrians and cyclists""",
 
-"""Signals (easy):
+'3':"""Signals (easy):
 What is the frequency of 3*sin(7*pi*t+3/8*pi)?
   A: 3.5 Hz
   B: 3.5 MHz
   C: 7 Hz
   D: 7 MHz""",
 
-"""Circuits (easy):
+'4':"""Circuits (easy):
 What is the total capacitance when two capacitors with a capacitance of 100uF are placed in parallel?
   A: 50uF
   B: 100uF
   C: 200uF
   D: 400uF""",
 
-"""Electronic circuits I (easy):
+'5':"""Electronic circuits I (easy):
 What formula is used to calculate the base current of a transistor?
   A: Ibase = (Vbase-emitter - Vbasebias)/Rbase
   B: Ibase = (Vbasebias - Vbase-emitter)/Rbase
   C: Ibase = Rbase/(Vbase-emitter - Vbasebias)
   D: Ibase = Rbase/(Vbasebias - Vbase-emitter)""",
 
-"""Calculus (easy):
+'6':"""Calculus (easy):
 What is the function of the tangent line of x^(3)+2x at x = 5?
   A: 2x
   B: x
   C: 36x - 125
   D: 77x - 250""",
 
-"""Engineering Design (easy):
+'7':"""Engineering Design (easy):
 When does the Engineering Design robot usually break?
   A: 3 days before the demo
   B: 1 day before the demo
   C: On the day of the demo
   D: It didn't break""",
 
-"""Physics (easy):
+'8':"""Physics (easy):
 How long does it take for a rock to hit the ground if it rolls off a cliff that is 30m high with a starting vertical velocity of 0m/s?
   A: 1.98432664372s
   B: 2.03285746782s
   C: 2.47309683415s
   D: 3.23428983287s""",
 
-"""Electromagnetics I (easy):
+'sante\n':"""Electromagnetics I (easy):
 What is the general expression for the lorentz force that is exerted by an electromagnetic field on a particle?
   A: F = qE + qv + B
   B: F = qE * qv * B
   C: F = qE * qv + B
-  D: F = qE + qv * B"""]
+  D: F = qE + qv * B"""}
 
-questionsDiff = ["""Electromagnetics II (hard):
+questionsDiff = {'0':"""Electromagnetics II (hard):
 Consider the time-harmonic electric field component of a uniform plane wave, given by E=E0sin(ωt−k0x+ϕ)azV/m
 Give an expression for the corresponding magnetic field component.
   A: H=−Z−10E0cos(ωt−k0x+ϕ) **ay**  A/m
@@ -81,28 +81,28 @@ Give an expression for the corresponding magnetic field component.
   C: H=−Z0E0sin(ωt−k0x+ϕ) **ay**  A/m
   D: the specified electric field is not consistent with Maxwell’s equations""",
 
-"""Mathematics II(hard):
+'1':"""Mathematics II(hard):
 Random variableXis uniform(−1,3) distributed.  Furthermore, letY=X2. For 1≤y≤9, the probability P(Y≤y) is given by
   A:12√y
   B:12(√y−1)
   C:14(√y−1)
   D:14(√y+ 1)""",
 
-"""Electronic Circuits II(hard):
+'2':"""Electronic Circuits II(hard):
 Calculate the effective capacitance and give the connections of the Miller capacitance. The Miller capacitance is 10uF and the gain of the MOSFET is 10x.
   A:Gate-Drain,110uF
   B:Source-Drain,110uF
   C:Gate-Drain,11uF
   D:Source-Drain,100uF""",
 
-"""Control Systems(hard):
+'3':"""Control Systems(hard):
 Consider a feedback control scheme , where G(s) =1s2+ 8s+ 7.A. Consider the controller D(s) = 1s(s+ 10).Compute the steady-state tracking error of the resulting closed loop system for a parabolic input.  Hint: use the final value theorem.
   A:55
   B:20
   C:70
   D:69""",
 
-"""Electromechanics(hard):
+'4':"""Electromechanics(hard):
 A series dc motor that can be treated as magnetically linear is operating at 1200 rpm with
 Vt = 250 V and Ia = 50 A. It is known that Ra + Rs = 0.2 Ω. If torque is increased until the speed is reduced to 750 rpm while Vt remains constant in value, determine the new value for Td.
   A:180.11Nm
@@ -110,14 +110,14 @@ Vt = 250 V and Ia = 50 A. It is known that Ra + Rs = 0.2 Ω. If torque is increa
   C:161.61Nm
   D:233.14Nm""",
 
-"""Wireless Components and Technlogies(hard):
+'5':"""Wireless Components and Technlogies(hard):
 Consider two celltowers 3 kilometers apart. One towers sends a message with a signals strength of 500mW at 5GHz. The antenna's have unit gain. What is the power received at the other antenna?
   A:1.00pW
   B:1.25pW
   C:2.00pW
   D.3.14pW""",
 
-"""Computation II(hard):
+'6':"""Computation II(hard):
 Imagine that you have trained your St. Bernard, Korg, to carry all Iron Man movies on Blu-Ray DVD's instead of Batman's. Each Blu-Ray DVD contains 5e10 bytes. The alien runs at 36 km/hour, but because he is made of stone and perpetually out of shape, every 10 seconds he needs to stop and rest for 5 seconds.
 
 What is the average datarate if Korg runs for 4000 meters? For simplicity, also take the last 5 seconds break when he reached his destination into account.
@@ -126,33 +126,33 @@ What is the average datarate if Korg runs for 4000 meters? For simplicity, also 
   C:3Gb/s
   D:5Gb/s""",
 
-"""Power Electonics(hard):
+'7':"""Power Electonics(hard):
 Figure https://imgur.com/qU709gu shows a step-up converter.  The input voltage Vd = 30V.  The converter is controlled to give a constant output voltage Vo= 80 V.  The inductance is L= 275μH. The switching frequency is 50kHz. The load resistance is 420Ω, in what mode of operation is this converter?
   A: CCM
   B: BCM
   C: DCM
   D: None""",
 
-"""Transmissionslines(hard):
+'8':"""Transmissionslines(hard):
 Consider a transmissionline with lenght L. The line impedance is 25Ω and it is terminated by a 75Ω load. The frequency is 3THz. What is the reflection coefficient?
   A:1
   B:0.5
   C:0
   D:π""",
 
-"""Thor(hard):
+'sante\n':"""Thor(hard):
 In the Marvel movies, what is the name of the guy with the red suit powered by a miniaturized Arc Reactor?
   A: Captain America
   B: Thor
   C: Iron Man
-  D: The Hulk"""]
+  D: The Hulk"""}
 
 
 
 
 
-answerEasy = ["B", "B", "A", "A", "C", "B", "D", "C", "C", "D"]
-answerDiff = ["B", "D", "A", "C", "D", "B", "A", "C", "B", "D"]
+answerEasy = {'0':"B", '1':"B", '2':"A", '3':"A", '4':"C", '5':"B", '6':"D", '7':"C", '8':"C", 'sante\n':"D"]
+answerDiff = {'0':"B", '1':"D", '2':"A", '3':"C", '4':"D", '5':"B", '6':"A", '7':"C", '8':"B", 'sante\n':"D"]
 codes = {'0':"forecast", '1':"neighbour", '2':"pledge", '3':"drawing", '4':"attitude", '5':"proclaim", '6':"judgment", '7':"mother", '8':"forestry", 'sante\n':"disorder"}
 
 routes = list()
